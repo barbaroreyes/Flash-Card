ffeb102244db02b1634fdb3006a39933b9a3e725
barbarof <barbarofreyes@gmail.com>
2021-02-04 14:43:37 -0600

    added deploy

---
+++ Project_Flashcards_Qualified_1/build/precache-manifest.3cecf789dfcc7d4851c8cd4ad354a7bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f869f6e0f0b8a26b520e861dbe00397a",
+    "url": "/Flash-Card/index.html"
+  },
+  {
+    "revision": "0652a5cf5bebabf662c9",
+    "url": "/Flash-Card/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "0652a5cf5bebabf662c9",
+    "url": "/Flash-Card/static/js/2.202565c4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Flash-Card/static/js/2.202565c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87b319309ac5e82d0f4d",
+    "url": "/Flash-Card/static/js/main.fd60ff14.chunk.js"
+  },
+  {
+    "revision": "e4f6a8ef84425cf443ea",
+    "url": "/Flash-Card/static/js/runtime-main.75a0ee0f.js"
+  }
+]);
