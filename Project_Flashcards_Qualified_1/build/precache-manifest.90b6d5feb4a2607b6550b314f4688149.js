0f1907edee4248c4e4a60f697123b1f381b9c06c
barbarof <barbarofreyes@gmail.com>
2021-02-10 19:11:42 -0600

    updatedeploy

--- Project_Flashcards_Qualified_1/build/precache-manifest.90b6d5feb4a2607b6550b314f4688149.js
+++ Project_Flashcards_Qualified_1/build/precache-manifest.90b6d5feb4a2607b6550b314f4688149.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f869f6e0f0b8a26b520e861dbe00397a",
+    "revision": "0fd37ccde47bbe386181b767b5e9f972",
     "url": "/Flash-Card/index.html"
   },
   {
-    "revision": "0652a5cf5bebabf662c9",
+    "revision": "c576179c2cad6d17f734",
     "url": "/Flash-Card/static/css/2.038644eb.chunk.css"
   },
   {
-    "revision": "0652a5cf5bebabf662c9",
-    "url": "/Flash-Card/static/js/2.202565c4.chunk.js"
+    "revision": "c576179c2cad6d17f734",
+    "url": "/Flash-Card/static/js/2.6262d0dc.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/Flash-Card/static/js/2.202565c4.chunk.js.LICENSE.txt"
+    "url": "/Flash-Card/static/js/2.6262d0dc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "87b319309ac5e82d0f4d",
-    "url": "/Flash-Card/static/js/main.fd60ff14.chunk.js"
+    "revision": "c67fd6a0da8f8b8a2dcb",
+    "url": "/Flash-Card/static/js/main.48dc10a3.chunk.js"
   },
   {
     "revision": "e4f6a8ef84425cf443ea",
